e4d3ce5a8e4d6dd2e3e8ef8e2e5b140b2442eb5b
DaleKim <97732494+muyaho-universe@users.noreply.github.com>
2023-04-03 14:18:47 +0900

    feat: create file reader

--- src/main/java/SmallScan.java
+++ src/main/java/SmallScan.java
@@ -1,2 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class SmallScan {
+    public static void main(String[] args) {
+        SmallScan smallScan = new SmallScan();
+        smallScan.run(args);
+    }
+
+    private void run(String[] args){
+        String testCaseString = "../test/" + args[0];
+        String str;
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(testCaseString));
+            while ((str = bufferedReader.readLine()) != null) {
+                String[] oneLine = str.split(" |\n|\t");
+                System.out.println(oneLine[1]);
+            }
+//            System.out.println(args[0]);
+        } catch (Exception e){
+            System.out.println("Failed to open the file");
+        }
+
+    }
 }
