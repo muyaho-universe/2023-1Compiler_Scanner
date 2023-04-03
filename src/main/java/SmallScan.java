211d8e043f1c772f19bbb179818bd22e41596951
DaleKim <97732494+muyaho-universe@users.noreply.github.com>
2023-04-03 17:42:10 +0900

    feat: scan method 작성중

--- src/main/java/SmallScan.java
+++ src/main/java/SmallScan.java
@@ -2,8 +2,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SmallScan {
+    ArrayList<Character> whiteSpace = new ArrayList<>(Arrays.asList(' ', '\n', '\t'));
     public static void main(String[] args) {
         SmallScan smallScan = new SmallScan();
         smallScan.run(args);
@@ -18,16 +21,27 @@ public class SmallScan {
             oneLine = new String(content);
             scan(oneLine);
         } catch (Exception e){
-            System.out.println(e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
 
     }
 
     private void scan(String oneLine){
         try {
+            String temp = "";
             for (int i = 0; i < oneLine.length(); i++){
-
+                Character oneChar = oneLine.charAt(i);
+                if (whiteSpace.contains(oneChar)){
+                    if(!temp.isBlank()){
+                        System.out.println(temp);
+                    }
+                    temp = "";
+                }
+                else {
+                    temp += oneChar;
+                }
             }
+
         } catch (Exception e) {
 
         }
